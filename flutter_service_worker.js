294833dbc4369e6c4fcd9fe15af3e2cce4538a74
arunkumar8085529 <arunkumar@thetopclassentertainment.com>
2025-04-16 02:16:44 +0530

    prod commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,7 +55,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "842465131253fa3da7eb3838a1eafe7d
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "9e5c3545170f1f78a028a2150578095c",
+"flutter_bootstrap.js": "d892da31ce8595c4fd724978ec778107",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
